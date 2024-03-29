ed73495f32f86e300711ba41b59bc36fff76e3e6
Vanessa Tang <vanesst5@uci.edu>
2024-03-29 15:29:38 -0700

    Removed data fetching

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,24 +2,24 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './Card.css'
 
-const Card = () => {
+const Card = ({data, name}) => {
 
-  const [total, setTotal] = useState(0)
+  // const [total, setTotal] = useState(0)
 
-  useEffect (() => {
-    const fetchTotal = () => {
-      axios.get('https://api.openbrewerydb.org/v1/breweries/meta')
-            .then((res) => {
-              setTotal(res.data.total)
-            })
-    }
-    fetchTotal()
-  }, [])
+  // useEffect (() => {
+  //   const fetchTotal = () => {
+  //     axios.get('https://api.openbrewerydb.org/v1/breweries/meta')
+  //           .then((res) => {
+  //             setTotal(res.data.total)
+  //           })
+  //   }
+  //   fetchTotal()
+  // }, [])
 
   return (
     <div className='card'>
-      <h2>{total}</h2>
-      <p>Total Number of Breweries</p>
+      <h2>{data}</h2>
+      <p>{name}</p>
     </div>
   )
 }
