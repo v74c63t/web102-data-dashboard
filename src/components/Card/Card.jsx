c0f5521b1992e4260c6b300ed1fa0fee9f025be4
Vanessa Tang <vanesst5@uci.edu>
2024-03-29 01:15:54 -0700

    Added some test data into card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,25 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './Card.css'
 
 const Card = () => {
+
+  const [total, setTotal] = useState(0)
+
+  useEffect (() => {
+    const fetchTotal = () => {
+      axios.get('https://api.openbrewerydb.org/v1/breweries/meta')
+            .then((res) => {
+              setTotal(res.data.total)
+            })
+    }
+    fetchTotal()
+  }, [])
+
   return (
     <div className='card'>
-      <div>DATA</div>
-      <div>NAME OF DATA</div>
+      <h2>{total}</h2>
+      <p>Total Number of Breweries</p>
     </div>
   )
 }
